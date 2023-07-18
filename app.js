09d29afc616812fb91f784cc58d9a410324fa55f
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2023-07-18 13:01:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-let gif = document.getElementById("gif");
+// let gif = document.getElementById("gif");
 
 
-gif.onclick = function () {
-    scrollTo(0, 1100);
-    alert("Src - TG: @arnolds_timofei4ik");
-}
+// gif.onclick = function () {
+//     scrollTo(0, 1100);
+//     alert("Src - TG: @arnolds_timofei4ik");
+// }
+
+
+let clck = document.getElementById('clck');
+let hide = document.getElementById('hide');
+let isVisible = false;
+
+
+clck.addEventListener('click', function() {
+
+    if (isVisible) {
+        hide.style.display = 'none';
+
+        isVisible = false;
+    } else {
+        hide.style.display = 'block';
+
+        isVisible = true;
+    }
+});
