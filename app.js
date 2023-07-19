ff8d2915caaa8fb63df90828df121cc994d0b6eb
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2023-07-19 21:43:14 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,20 +7,53 @@
 // }
 
 
-let clck = document.getElementById('clck');
-let hide = document.getElementById('hide');
-let isVisible = false;
+// let clck = document.getElementById('clck');
+// let hide = document.getElementById('hide');
+// let isVisible = false;
 
 
-clck.addEventListener('click', function() {
+// clck.addEventListener('click', function() {
 
-    if (isVisible) {
-        hide.style.display = 'none';
+//     if (isVisible) {
+//         hide.style.display = 'none';
 
-        isVisible = false;
-    } else {
-        hide.style.display = 'block';
+//         isVisible = false;
+//     } else {
+//         hide.style.display = 'block';
 
-        isVisible = true;
-    }
-});
+//         isVisible = true;
+//     }
+// });
+const overlay = document.getElementById('overlay');
+let nightClck = document.querySelector('img.night');
+let sunClck = document.querySelector('img.sun');
+sunClck.style.visibility = 'hidden';
+nightClck.style.visibility = 'visible';
+
+function showOverlay() {
+  overlay.style.display = 'block';
+}
+
+function hideOverlay() {
+  overlay.style.display = 'none';
+}
+
+nightClck.addEventListener("click", function () {
+    overlay.style.display = 'block';
+    sunClck.style.visibility = 'visible';
+    nightClck.style.visibility = 'hidden';
+    sunClck.style.zIndex = '1';
+    
+    // nightClck.src = 'sun.png';
+    // nightClck.alt = 'Sun';
+})
+
+sunClck.addEventListener("click", function () {
+    overlay.style.display = 'none';
+    sunClck.style.visibility = 'hidden';
+    
+    nightClck.style.visibility = 'visible';
+
+    // sunClck.src = 'night.png';
+    // sunClck.alt = 'Night';
+})
